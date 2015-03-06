1ab88959d81a19227e0c4b65f7e440b31c16ba99
Nate Holland <nateh@spiceworks.com>
2015-03-06 11:24:33 -0600

    adding checks the letter threshold test

--- test/lib/rubycritic/report_generators/global_rating_calculator_test.rb
+++ test/lib/rubycritic/report_generators/global_rating_calculator_test.rb
@@ -1,6 +1,7 @@
 require "test_helper"
 require "rubycritic/generators/global_rating_calculator"
 require "rubycritic/core/analysed_module"
+require "rubycritic/configuration"
 
 describe Rubycritic::Generator::GlobalRatingCalculator do
   include Rubycritic::Generator::GlobalRatingCalculator
@@ -13,5 +14,18 @@ describe Rubycritic::Generator::GlobalRatingCalculator do
     analysed_modules = [am1, am2]
     gpa = calculate_gpa(analysed_modules)
     gpa.round(2).must_equal 2.14
+  end
+
+  it "checks the letter threshold and exits" do
+    orig_stderr = $stderr.clone
+    $stderr.reopen File.new("/dev/null", "w")
+    Rubycritic::Config.set(:letter_threshold => 8.0)
+    smells1 = [SmellDouble.new(:cost => 1), SmellDouble.new(:cost => 2)]
+    smells2 = [SmellDouble.new(:cost => 100), SmellDouble.new(:cost => 100)]
+    am1 = Rubycritic::AnalysedModule.new(:smells => smells1, :complexity => 0, :lines => 25)
+    am2 = Rubycritic::AnalysedModule.new(:smells => smells2, :complexity => 0, :lines => 10)
+    analysed_modules = [am1, am2]
+    assert_raises(SystemExit) { check_letter_threshold(analysed_modules) }
+    $stderr.reopen orig_stderr
   end
 end
